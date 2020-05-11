fa00be0bc076ca5d42cb4f767cc3d2c20e6e63b2
nawafChowdhuryAkash <nawafc959@gmail.com>
2020-05-12 00:43:18 +0600

    11854

---
+++ 11854/11854/main.cpp
@@ -0,0 +1,33 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a , b , c ;
+    
+    while(1){
+        cin >> a >> b  >> c ;
+        
+        
+        if (a == 0 && b == 0 && c == 0){
+            break;
+        }
+        
+        int A , B ,C;
+        A = a*a ;
+        B = b*b;
+        C = c*c ;
+        
+        
+        
+        if ((A+B == C) || (B+C == A) || (C+A == B)){
+            cout << "right" << endl;
+        }else{
+            cout << "wrong" << endl ;
+        }
+        
+    }
+    
+}
