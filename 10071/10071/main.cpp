56517103bb459990bc52b171b3a4af1e1351c153
nawafChowdhuryAkash <nawafc959@gmail.com>
2020-05-17 20:36:07 +0600

    10071

---
+++ 10071/10071/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  10071
+//
+//  Created by as on 5/16/20.
+//  Copyright © 2020 as. All rights reserved.
+//
+
+#include <iostream>
+using namespace std ;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+   long a ,b  ;
+    
+    while (cin >> a >> b) {
+        
+        
+        
+        long B = b*2 ;
+        
+        cout << a*B << endl ;
+        
+    }
+}
